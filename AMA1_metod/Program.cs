47dbad6763cea3e4264c6a2261bd89d86d96fc74
rumeysavarli23@gmail.com <rumeysavarli23@gmail.com>
2025-04-09 15:11:51 +0300

    generic

--- AMA1_metod/Program.cs
+++ AMA1_metod/Program.cs
@@ -1,4 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// See https://aka.ms/new-console-template for more informationo
 using AMA1_metod.Abstract;
 using AMA1_metod.Classes;
 using AMA1_metod.Composition;
